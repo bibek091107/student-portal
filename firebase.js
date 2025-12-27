d78fe9b15631b3c977ca29d29718842a22a48736
Bibek Khadka <dvmqk4z68g@privaterelay.appleid.com>
2025-12-27 16:19:09 +0530

    update

--- firebase.js
+++ firebase.js
@@ -1,9 +1,6 @@
-// Import Firebase co { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Import Firebase Authentication
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-// Firebase configuration (YOUR PROJECT)
 const firebaseConfig = {
   apiKey: "AIzaSyCqAA39CbpDLXRU9OQ4T1TaKDGs_iPPceE",
   authDomain: "student-management-syste-e3edc.firebaseapp.com",
@@ -13,8 +10,7 @@ const firebaseConfig = {
   appId: "1:674803364755:web:ffd5e3e3a852d3624fae66"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Authentication
-export const auth = getAuth(app);
+// Export Firestore
+export const db = getFirestore(app);
