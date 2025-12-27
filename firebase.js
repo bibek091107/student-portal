d26d44f2d9c0b14745bd23323b1750966a0ec456
Bibek Khadka <dvmqk4z68g@privaterelay.appleid.com>
2025-12-27 14:28:36 +0530

    Firebase update

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import Firebase core
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+// Import Firebase Authentication
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+// Firebase configuration (YOUR PROJECT)
+const firebaseConfig = {
+  apiKey: "AIzaSyCqAA39CbpDLXRU9OQ4T1TaKDGs_iPPceE",
+  authDomain: "student-management-syste-e3edc.firebaseapp.com",
+  projectId: "student-management-syste-e3edc",
+  storageBucket: "student-management-syste-e3edc.appspot.com",
+  messagingSenderId: "674803364755",
+  appId: "1:674803364755:web:ffd5e3e3a852d3624fae66"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Authentication
+export const auth = getAuth(app);
