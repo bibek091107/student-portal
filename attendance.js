db53b40286e3ab1c7e8a81baf0612b255ab4c3f0
Bibek Khadka <dvmqk4z68g@privaterelay.appleid.com>
2025-12-31 01:49:11 +0545

    Update attendance.js

--- attendance.js
+++ attendance.js
@@ -1,28 +1,28 @@
-// const totalDays = 150;
-// const attendancePercent = 82;
+const totalDays = 150;
+const attendancePercent = 82;
 
-// const presentDays = Math.round((attendancePercent / 100) * totalDays);
-// const absentDays = totalDays - presentDays;
+const presentDays = Math.round((attendancePercent / 100) * totalDays);
+const absentDays = totalDays - presentDays;
 
-// const circle = document.getElementById("attendanceCircle");
-// const tooltip = document.getElementById("attendanceTooltip");
-// const percentText = document.getElementById("attendancePercent");
+const circle = document.getElementById("attendanceCircle");
+const tooltip = document.getElementById("attendanceTooltip");
+const percentText = document.getElementById("attendancePercent");
 
-// if (circle && tooltip && percentText) {
-//   percentText.innerText = attendancePercent + "%";
+if (circle && tooltip && percentText) {
+  percentText.innerText = attendancePercent + "%";
 
-//   circle.style.background = `
-//     conic-gradient(
-//       #22c55e 0% ${attendancePercent}%,
-//       #ef4444 ${attendancePercent}% 100%
-//     )
-//   `;
+  circle.style.background = `
+    conic-gradient(
+      #22c55e 0% ${attendancePercent}%,
+      #ef4444 ${attendancePercent}% 100%
+    )
+  `;
 
-//   tooltip.innerHTML = `
-//     Present: ${presentDays} Days<br>
-//     Absent: ${absentDays} Days
-//   `;
+  tooltip.innerHTML = `
+    Present: ${presentDays} Days<br>
+    Absent: ${absentDays} Days
+  `;
 
-//   circle.addEventListener("mouseenter", () => tooltip.style.opacity = "1");
-//   circle.addEventListener("mouseleave", () => tooltip.style.opacity = "0");
-// }
+  circle.addEventListener("mouseenter", () => tooltip.style.opacity = "1");
+  circle.addEventListener("mouseleave", () => tooltip.style.opacity = "0");
+}
